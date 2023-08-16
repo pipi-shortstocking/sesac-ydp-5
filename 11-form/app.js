937e4ed5dad62480c4c575f77da6fc4c92a91129
pipi-shortstocking <jannie435@naver.com>
2023-08-16 16:24:26 +0900

    study: form 유효성 검사, falsy, truthy

--- 11-form/app.js
+++ 11-form/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.set('view engine', 'ejs');
-app.set('/views', 'views');
+app.set("view engine", "ejs");
+app.set("/views", "views");
 // 미들웨어(middleware)
 // : 요청(req)과 응답(res)의 중간에서 작업하는 코드
 // app.use()
@@ -14,30 +14,32 @@ app.use(express.json()); // json 형식으로 데이터를 주고 받음
 
 // 라우팅(Routing) - 주소 설정
 // GET '/' => index.ejs 를 보여줌
-app.get('/', (req, res) => {
-    console.log(req.query);
-    // res.render(ejs_경로, 데이터)
-    // ejs_경로: views/ 폴더 내부 ejs 파일의 주소
-    // 데이터: 뷰에 넣어줄 정보
-    res.render('index', { title: '폼 전송을 연습해보자!' });
+app.get("/", (req, res) => {
+  console.log(req.query);
+  // res.render(ejs_경로, 데이터)
+  // ejs_경로: views/ 폴더 내부 ejs 파일의 주소
+  // 데이터: 뷰에 넣어줄 정보
+  //   res.render("index", { title: "폼 전송을 연습해보자!" });
+  res.render("index", { title: "실습2. post로 정보받기" });
 });
 
 // GET '/getForm' => 임의의 메세지 전송
 // get 방식은 클라이언트에서 보낸 데이터가 req.query에 저장
-app.get('/getForm', (req, res) => {
-    console.log(req.query); // { id: 'apple', pw: '1234' }
-    // res.send('get 요청 성공!');
-    res.render('result', { title: 'Get 요청', userInfo: req.query });
+app.get("/getForm", (req, res) => {
+  console.log(req.query); // { id: 'apple', pw: '1234' }
+  // res.send('get 요청 성공!');
+  res.render("result", { title: "Get 요청", userInfo: req.query });
 });
 
 // POST '/postForm' => 임의의 메세지 전송
 // post 방식은 클라이언트에서 보낸 데이터가 req.body에 저장
-app.post('/postForm', (req, res) => {
-    console.log(req.body); // { id: 'minion', pw: '4321' }
-    // res.send('post 요청 성공!');
-    res.render('result', { title: 'Post 요청', userInfo: req.body });
+app.post("/postForm", (req, res) => {
+  console.log(req.body); // { id: 'minion', pw: '4321' }
+  // res.send('post 요청 성공!');
+  //   res.render("result", { title: "Post 요청", userInfo: req.body });
+  res.render("result", { title: "실습2", userInfo: req.body });
 });
 
 app.listen(PORT, () => {
-    console.log(`${PORT} is opening!`);
+  console.log(`${PORT} is opening!`);
 });
