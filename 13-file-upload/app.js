5fd140b9258c73beffe5de9983d3875bfcf71c08
pipi-shortstocking <jannie435@naver.com>
2023-08-21 15:32:11 +0900

    study: multer 세부 설정

--- 13-file-upload/app.js
+++ 13-file-upload/app.js
@@ -4,8 +4,27 @@ const PORT = 8000;
 
 // multer 관련 설정 코드
 const multer = require("multer");
+const path = require("path"); // 경로에 관한 내장 모듈
 const upload = multer({
   dest: "uploads/", // dest: 클라이언트가 업로드한 파일을 저장할 서버측 경로
+});
+// multer 세부 설정
+const uploadDetail = multer({
+  // storage: 저장할 공간에 대한 정보
+  // done(null, xx) 여기서 null은 error를 의미하는 매개변수
+  // 에러가 없으므로 "null"이라고 전달하여 콜백 함수를 호출!
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      // done: callback
+      done(null, "uploads/"); // 파일을 업로드할 경로 설정
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname); // 파일 "확장자"를 추출
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  // limits: 파일 제한 정보
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
 
 app.set("view engine", "ejs");
@@ -21,22 +40,24 @@ app.get("/", (req, res) => {
 // upload.single("userfile"): 클라이언트의 요청이 들어오면
 // multer 설정(upload 변수)에 따라 파일을 업로드 한 후, req.file 객체 생성
 // single() 인자는 input 태그의 name 속성과 일치시켜야 함
-app.post("/upload", upload.single("userfile"), (req, res) => {
+// app.post("/upload", upload.single("userfile"), (req, res) => {
+app.post("/upload", uploadDetail.single("userfile"), (req, res) => {
   // req.file: 파일 업로드 정보
   // req.body: 파일 외의 정보들
   console.log(req.file);
   console.log(req.body);
   res.send("파일 업로드 완료!");
 
+  // req.file 객체 자세히 보기
   // {
-  //     fieldname: 'userfile',
-  //     originalname: 'apple1.png',
-  //     encoding: '7bit',
-  //     mimetype: 'image/png',
-  //     destination: 'uploads/',
-  //     filename: '7399f417e8d2f61ec9456409d570eacf',
-  //     path: 'uploads/7399f417e8d2f61ec9456409d570eacf',
-  //     size: 2515
+  //     fieldname: 'userfile', // 폼에 정의한 name 값
+  //     originalname: 'apple1.png', // 원본 파일명
+  //     encoding: '7bit', // 파일 인코딩 타입
+  //     mimetype: 'image/png', // 파일 타입
+  //     destination: 'uploads/', // 파일 저장 경로
+  //     filename: '7399f417e8d2f61ec9456409d570eacf', // destination 저장된 파일명
+  //     path: 'uploads/7399f417e8d2f61ec9456409d570eacf', // 업로드된 파일 전체 경로
+  //     size: 2515 // 파일 크기
   //   }
 });
 
