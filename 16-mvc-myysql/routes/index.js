6288244ab2afea03cdec2771a1944ebd78adae05
pipi-shortstocking <jannie435@naver.com>
2023-08-28 14:00:59 +0900

    study: mvc mysql 연결 전 기초 프로젝트

---
+++ 16-mvc-myysql/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const controller = require("../controller/Cvisitor");
+const router = express.Router();
+
+router.get("/", controller.main);
+router.get("/visitors", controller.getVisitors);
+
+module.exports = router;
