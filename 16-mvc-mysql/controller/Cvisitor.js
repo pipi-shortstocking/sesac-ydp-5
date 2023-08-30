8bf010b8d1e722a6feb47c493422c8acec429c44
pipi-shortstocking <jannie435@naver.com>
2023-08-30 15:17:45 +0900

    study; mvc with mysql read one, update

--- 16-mvc-mysql/controller/Cvisitor.js
+++ 16-mvc-mysql/controller/Cvisitor.js
@@ -11,6 +11,9 @@ exports.getVisitors = (req, res) => {
   // [after]
   // console.log(Visitors.getVisitors());
   Visitor.getVisitors((result) => {
+    // id 기준으로 내림차순으로 보여지도록
+    // result = result.reverse();
+
     console.log("controller >>", result);
     res.render("visitor", { data: result });
   });
@@ -33,5 +36,25 @@ exports.deleteVisitor = (req, res) => {
   Visitor.deleteVisitor(id, (result) => {
     console.log("controller >>", result); // true
     res.send(result); // res.send(true)
+  });
+};
+
+exports.getVisitor = (req, res) => {
+  // console.log(req.query);
+  console.log(req.params); // { id: 5 }
+  const { id } = req.params;
+
+  Visitor.getVisitor(id, (result) => {
+    // result: 모델의 getVisitor callback의 인자(rows[0])
+    console.log(result); // {}
+    res.send(result);
+  });
+};
+
+exports.updateVisitor = (req, res) => {
+  console.log(req.body); // {id: x, name: x, comment: x}
+
+  Visitor.updateVisitor(req.body, () => {
+    res.send({ isUpdated: true });
   });
 };
