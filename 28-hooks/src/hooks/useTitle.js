54b138c6a40338b16463c6a43b464613679d97ad
pipi-shortstocking <jannie435@naver.com>
2023-10-06 18:39:39 +0900

    study: react hooks 이해하기

---
+++ 28-hooks/src/hooks/useTitle.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+// custom hooks
+export default function useTitle(title) {
+  useEffect(() => {
+    const prevTitle = document.title;
+    document.title = title;
+
+    console.log(prevTitle);
+    console.log(document.title);
+
+    return () => (document.title = prevTitle);
+    // return () => (document.title = title);
+  }, [title]);
+}
