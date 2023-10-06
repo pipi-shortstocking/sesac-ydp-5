cb186e0c146782563b4afc39cf72e99e4f1e0e70
pipi-shortstocking <jannie435@naver.com>
2023-10-06 19:06:37 +0900

    question: usetitle

--- 28-hooks/src/hooks/useTitle.js
+++ 28-hooks/src/hooks/useTitle.js
@@ -3,13 +3,13 @@ import { useEffect } from 'react';
 // custom hooks
 export default function useTitle(title) {
   useEffect(() => {
-    const prevTitle = document.title;
+    // const prevTitle = document.title;
     document.title = title;
 
-    console.log(prevTitle);
+    // console.log(prevTitle);
     console.log(document.title);
 
-    return () => (document.title = prevTitle);
-    // return () => (document.title = title);
+    // return () => (document.title = prevTitle);
+    return () => (document.title = title);
   }, [title]);
 }
