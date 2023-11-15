374fb6f63bb92c70b98cf7cf66be45bee96112c0
pipi-shortstocking <jannie435@naver.com>
2023-11-15 18:59:22 +0900

    practice: my chat

--- 35-socket.io/server.js
+++ 35-socket.io/server.js
@@ -8,7 +8,7 @@ const io = socketIO(server); // socket.io를 http 서버에 연결
 const PORT = 8080;
 
 // 사용자 닉네임 모음 객체
-const nickObjs = {}; // {socket.id: nick1, socket.id: nick2 ...}
+const nickObjs = {}; // { socket.id: nick1, socket.id: nick2, ...  }
 
 app.set('view engine', 'ejs');
 
@@ -28,60 +28,68 @@ io.on('connection', (socket) => {
   console.log('서버 연결 완료 :: ', socket.id);
   // socket.id: 소켓 고유 아이디 (브라우저 탭 단위)
 
-  //   // [실습 1]
+  // [실습1]
   //   socket.on('hello', (data) => {
-  //     console.log(data);
+  //     console.log(`${data.who} : ${data.msg}`);
   //     socket.emit('helloKr', { who: 'hello', msg: '안녕!!!!' });
   //   });
 
-  //   socket.on('study', (data) => {
-  //     console.log(data);
-  //     socket.emit('studyKr', { who: 'study', msg: '공부 중!!!!' });
-  //   });
+  // [실습3] 채팅창 입장 안내 문구
+  //   io.emit('notice', `${socket.id} 님이 입장하셨습니다.`);
 
-  //   socket.on('bye', (data) => {
-  //     console.log(data);
-  //     socket.emit('byeKr', { who: 'bye', msg: '잘 가요!!!!' });
-  //   });
-
-  // [실습 2]
-  socket.on('client-send', (data) => {
-    console.log(data.msg);
-    socket.emit('sendKr', { msg: `${data.msg}` });
-  });
-
-  // [실습 3] 채팅장 입장 안내 문구
-  //   io.emit('notice', `${socket.id}님이 입장하셨습니다.`);
-
-  // [실습 3-2]
+  // [실습3-2] 채팅창 입장 문구 socket.id -> nickname
   // emit() from server
   // - socket.emit(event_name, data): 해당 클라이언트에게만 이벤트, 데이터를 전송
   // - io.emit(event_name, data): 서버에 접속된 모든 클라이언트 전송
+  // - io.to(소켓아이디).emit(event_name, data): 소켓 아이디에 해당하는 클라이언트에게만 전송
   socket.on('setNick', (nick) => {
+    console.log(`닉네임 설정 완료 :: ${nick} 님 입장`);
+
     // 프론트에서 입력한 nick이 nickObjs 객체에 존재하는지 검사
-    // 이미 존재; error 이벤트 + '이미 존재하는 닉네임입니다'
+    // 이미 존재; error 이벤트 + '이미 존재하는 닉네임 입니다' => 클라이언트; error 이벤트 받으면 alert 띄우기
     // 새 닉네임; notice 이벤트 + ${nick} 님이 입장하셨습니다.
     if (Object.values(nickObjs).indexOf(nick) > -1) {
       // 이미 존재하는 닉네임이 있음
-      socket.emit('error', `이미 존재하는 닉네임입니다.`);
+      socket.emit('error', '이미 존재하는 닉네임입니다.');
     } else {
       // 새로운 닉네임
       nickObjs[socket.id] = nick;
       console.log('접속 유저 목록 :: ', nickObjs);
-      io.emit('notice', `${nick}님이 입장하셨습니다.`); // 전체 공지
+      io.emit('notice', `${nick} 님이 입장하셨습니다.`); // 전체 공지
       socket.emit('entrySuccess', nick); // 해당 소켓 데이터 전송
       updateList();
     }
   });
 
-  // [실습 3-3] 클라이언트 퇴장시
+  // [실습3-3] 클라이언트 퇴장시
   // "notice" 이벤트로 퇴장 공지
   socket.on('disconnect', () => {
-    console.log('접속 끊김:: ', socket.id);
+    if (nickObjs[socket.id]) {
+      console.log(
+        '접속 끊김 :: ',
+        `${nickObjs[socket.id]} 님 퇴장 :: `,
+        socket.id
+      );
 
-    io.emit('notice', `${nickObjs[socket.id]} 님이 퇴장하셔습니다.`);
+      io.emit('notice', `${nickObjs[socket.id]} 님이 퇴장하셨습니다.`);
+    } else {
+      socket.id = '알 수 없음';
+
+      console.log('접속 끊김 :: ', `${socket.id} 님 퇴장 :: `, socket.id);
+
+      io.emit('notice', `${socket.id} 님이 퇴장하셨습니다.`);
+    }
+
     delete nickObjs[socket.id];
     updateList();
+  });
+
+  // [실습 4] 채팅창 메세지 전송 Step1
+  // send 이벤트를 받아서
+  // 모두에게 newMessage 이벤트로 {닉네임, 입력창내용} 데이터를 전송
+  socket.on('send', (data) => {
+    // { nickName: 'aa', msg: 'aaass' }
+    io.emit('newMessage', data);
   });
 });
 
